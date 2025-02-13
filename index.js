9eaee596b678d2c1d57c99f082703cf560ab514b
khanya jara <khanyajara@gmail.com>
2025-02-13 12:16:27 +0200

    installed cors

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-require('dotenv').config();  // Add this line at the top
+require('dotenv').config();  // Load environment variables from a .env file
 
 const express = require('express');
 const { PDFNet } = require('@pdftron/pdfnet-node');
+const cors = require('cors');  // Import CORS package
 
 const app = express();
 const port = 8971;
 
-// Initialize PDFTron SDK with license key from environment variable
+app.use(cors()); 
+
 const licenseKey = process.env.YOUR_LICENSE_KEY;
 PDFNet.initialize(licenseKey);
 
@@ -14,12 +16,16 @@ app.get('/generate-pdf', async (req, res) => {
     try {
         const main = async () => {
             const doc = await PDFNet.PDFDoc.create();
+            
             const page = await doc.pageCreate();
             doc.pagePushBack(page);
+
             await doc.save('blank.pdf', PDFNet.SDFDoc.SaveOptions.e_linearized);
+
             res.contentType('application/pdf');
             res.sendFile('blank.pdf', { root: '.' });
         };
+
         await PDFNet.runWithCleanup(main);
 
     } catch (err) {
@@ -28,6 +34,7 @@ app.get('/generate-pdf', async (req, res) => {
     }
 });
 
+// Start the server
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
